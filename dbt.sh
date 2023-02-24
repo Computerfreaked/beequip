59ead0609df974c7a408f22dc13622c5f10be381
Computerfreaked <bartjanjdevries@gmail.com>
2023-02-24 09:02:42 +0100

    folder change

--- dbt.sh
+++ dbt.sh
@@ -1,5 +1,5 @@
 sudo docker run --name beequip-dbt -it --rm \
---mount type=bind,source=/home/bartjan/beequip-assessment/dbt/beequip_dbt_project,target=/usr/app \
---mount type=bind,source=/home/bartjan/beequip-assessment/dbt,target=/root/.dbt/ \
+--mount type=bind,source=/home/bartjan/beequip/dbt/beequip_dbt_project,target=/usr/app \
+--mount type=bind,source=/home/bartjan/beequip/dbt,target=/root/.dbt/ \
 ghcr.io/dbt-labs/dbt-postgres:1.4.1 \
 run
